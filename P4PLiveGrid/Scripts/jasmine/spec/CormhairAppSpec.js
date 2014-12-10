b5cf6585380673f1ed80e4595dd18966c7d6e03f
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-12-10 15:41:02 -0500

    added in jasmine javascript testing library
    add tests for cormhair angular app

---
+++ P4PLiveGrid/Scripts/jasmine/spec/CormhairAppSpec.js
@@ -0,0 +1,34 @@
+﻿// Reference Scripts/app/controller.js
+
+describe('cormhairCtrl', function() {
+  var $httpBackend;
+  beforeEach(module('cormhairApp'));
+
+  var $controller;
+
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  beforeEach(inject(function(_$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+  }));
+    
+  describe('Loading JSON', function() {
+    it('Loads JSON with a Title', function() {
+      var $scope = {};
+      var controller = $controller('cormhairCtrl', { $scope : $scope })
+      $httpBackend.expectGET('/Scripts/KPIInfo.json').respond({
+        "P4PInfoId":1, 
+        "ProgramName":"TCM", 
+        "KpiId": "TCM-01-01:",
+        "Heading": "Rationale", 
+        "Text" : "To measure the continuity of service provided to CBH-funded TCM and ACT members."
+      });
+      $httpBackend.flush();
+      expect($scope.data.Heading).toEqual("Rationale");
+      
+    });
+  });
+});
---
+++ P4PLiveGrid/Scripts/jasmine/spec/DashboardSpec.js
@@ -0,0 +1,30 @@
+﻿//Reference dashboard-1.0.0.js
+jasmine.getFixtures().fixturesPath = '/';
+describe("fixDates()", function() {
+  it("should return an array of fixed dates", function() {
+    var encounterData = [{ "P4PSummaryId": 1, 
+                            "ProgramName": "TCM", 
+                            "KpiId": "TCM-01-01", 
+                            "Clinician": "Nancy Jones", 
+                            "PatientId": "101123", 
+                            "PatientName": "Joe Balagio", 
+                            "EncounterStartDate": "2014-01-05T00:00:00", 
+                            "EncounterEndDate": "2014-02-10T00:00:00", 
+                            "ElapsedDays": 34 
+                            }]
+    fixedDates = fixDates(encounterData);
+    expect(fixedDates[0].EncounterStartDate instanceof Date).toBe(true)
+
+  });
+});
+
+//describe("colorData()", function() {
+//  it("should color in the time elapsed column", function() {
+//    loadFixtures('index.html');
+//    colorData();
+//    firstDay = $('.elapsedDays')[0]
+//    expect(firstDay).nottoHaveCss({background: '#ffffff'});
+//  });
+//});
+//
+
