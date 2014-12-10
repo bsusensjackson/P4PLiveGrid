76127594ed35b1b7f82557e2083bf3cbfc68b6f2
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-12-10 08:36:14 -0500

    initial

---
+++ P4PLiveGrid/Scripts/addDataGrid.js
@@ -0,0 +1,57 @@
+var fixDates = function (encounterData) {
+  betterDates = encounterData.map(function (data) {
+    data["EncounterStartDate"] = new Date(data["EncounterStartDate"]);
+    data["EncounterEndDate"] = new Date(data["EncounterEndDate"]);
+    return data;
+  });
+  return betterDates;
+};
+
+ function colorData() {
+  var $elements = $('.elapsedDays');
+  $elements.each(function(){
+    if ($(this).text() > 30) {
+      $(this).css("background", "#CC0000");
+      $(this).css("color", "#ffffff");
+    } else if ($(this).text() > 15 && $(this).text() < 30) {
+      $(this).css("background", "yellow");
+    };
+  });
+};
+
+var setGrid = function(container) {
+  container.dxDataGrid({
+    dataSource: fixedDataSet,
+    columns: [
+      { dataField: 'Clinician' },
+      { dataField: 'PatientId' },
+      { dataField: 'PatientName' },
+      { dataField: 'EncounterStartDate', format: 'shortDate', allowFiltering: true},
+      { dataField: 'EncounterEndDate', format: 'shortDate', allowFiltering: true},
+      { dataField: 'ElapsedDays', cssClass: 'elapsedDays'}
+    ], 
+    columnChooser: { enabled: true },
+    filterRow: { visible: true },
+    pager: { visible: true },
+    paging: { pageSize: 7 },
+    contentReadyAction: function(e) {
+    e.element.find('.dx-header-row .dx-datagrid-action-cursor')
+    .on('click', function() { 
+      setTimeout(function(){ 
+        colorData();
+      }, 100)
+    e.element.find('.dx-dropdowneditor-input-wrapper')
+    .on('click', function() {
+      setTimeout(function(){
+        colorData();
+      }, 100)
+    })
+       
+    });
+  },
+    width: function(){
+      return "100%";
+    }
+  });
+};
+
---
+++ P4PLiveGrid/Scripts/app/controller.js
@@ -0,0 +1,21 @@
+﻿var funApp = angular.module('funApp', []);
+
+funApp.controller('myCont', function($scope, $http){
+  $http.get('/Scripts/KPIInfo.json').success(function(data) {
+    $scope.data = angular.fromJson(data);
+  });
+ });
+angular.module('funApp').directive('jqueryCreate', function() {
+  return {
+    link: function($scope, $element, $attr) {
+        $element.parent().trigger('create');
+     }
+  };
+ 
+});
+  
+
+
+
+
+
